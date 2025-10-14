021577eabdff6c968e5031acdd52c5ddacd9031c
9e35ff <yagasakiwanderlust@proton.me>
2025-10-14 20:13:50 -0300

    Update f0_onlyPumpkin.py

--- f0_onlyPumpkin.py
+++ f0_onlyPumpkin.py
@@ -7,11 +7,14 @@ def correct_ground_for(crop):
 
 
 def handle_tile(crop, replant_dead=False):
+    # comment this harvest() if you want a big pumpkin
+    harvest()
     if Items.Weird_Substance:
         use_item(Items.Weird_Substance)
         
-    if can_harvest():
+    if get_entity_type() != Entities.Pumpkin:
         harvest()
+        plant(Entities.Pumpkin)
 
     correct_ground = correct_ground_for(crop)
     if get_ground_type() != correct_ground:
@@ -36,7 +39,7 @@ while True:
     if get_entity_type() == Entities.Treasure:
         harvest()
 
-    elif y in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14):
+    elif y in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1):
         if can_harvest() or Entities.Dead_Pumpkin:
             handle_tile(Entities.Pumpkin)
         move_next()
