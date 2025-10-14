a39f3b435860f5f7a2045ca08cb310f1d6d17dbe
9e35ff <yagasakiwanderlust@proton.me>
2025-10-14 12:58:57 -0300

    Create f0_onlyPumpkin.py

---
+++ f0_onlyPumpkin.py
@@ -0,0 +1,48 @@
+def correct_ground_for(crop):
+    if crop in (Entities.Tree, Entities.Grass):
+        return Grounds.Grassland
+    return Grounds.Soil
+
+
+def handle_tile(crop, replant_dead=False):
+    if Items.Weird_Substance:
+        use_item(Items.Weird_Substance)
+        
+    if can_harvest():
+        harvest()
+
+    correct_ground = correct_ground_for(crop)
+    if get_ground_type() != correct_ground:
+        till()
+
+    if replant_dead and Entities.Dead_Pumpkin:
+        plant(crop)
+    else:
+        plant(crop)
+
+    if Items.Water:
+        use_item(Items.Water)
+        
+    if Items.Fertilizer:
+        use_item(Items.Fertilizer)
+
+def move_next():
+    move(North)
+
+while True:
+    y = get_pos_y()
+    if get_entity_type() == Entities.Treasure:
+        harvest()
+
+    elif y in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
+        if can_harvest() or Entities.Dead_Pumpkin:
+            handle_tile(Entities.Pumpkin)
+        move_next()
+
+
+    elif y == 11:
+        if can_harvest() or Entities.Dead_Pumpkin:
+            handle_tile(Entities.Pumpkin)
+        move(East)
+        move(North)
+        
